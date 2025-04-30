34066d85c5c60aff79ac0d066fba6a3c017ce06a
bsahd <193844131+bsahd@users.noreply.github.com>
2025-04-30 15:49:21 +0900

    format

--- backend/index.js
+++ backend/index.js
@@ -10,21 +10,30 @@ app.use(express.static(path.join(import.meta.dirname, "..", "dist")));
 const httpServer = createServer(app);
 const io = new Server(httpServer, {});
 
-let page = JSON.parse((await fs.readFile(path.join(import.meta.dirname, "page"))).toString());
+let page = JSON.parse(
+	(await fs.readFile(path.join(import.meta.dirname, "page"))).toString(),
+);
 
 io.on("connect", (sock) => {
 	console.log("new connect");
 	sock.emit("pageReplace", page);
 	sock.on("pageEdit", async (data) => {
 		page = patchDiff(page, data);
-		await fs.writeFile(path.join(import.meta.dirname, "page"), JSON.stringify(page));
+		await fs.writeFile(
+			path.join(import.meta.dirname, "page"),
+			JSON.stringify(page),
+		);
 		sock.broadcast.emit("pageEdit", data);
 	});
 });
 
 app.get("*a", async (req, res) => {
 	res.type("html");
-	res.send(await fs.readFile(path.join(import.meta.dirname, "..", "dist", "index.html")));
+	res.send(
+		await fs.readFile(
+			path.join(import.meta.dirname, "..", "dist", "index.html"),
+		),
+	);
 });
 
 httpServer.listen(3000);
